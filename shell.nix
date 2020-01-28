b89106f818c2c2810cd3f0ae6086e3fa21f182bf
Terje Larsen <terlar@gmail.com>
2020-01-28 10:06:40 +0100

    dev: improve environment

--- shell.nix
+++ shell.nix
@@ -10,11 +10,24 @@ in
 with (import pkgsPath { inherit overlays; });
 
 let
+  emacsInit = writeText "init.el" ''
+    (set-face-attribute 'default nil :height 150)
+    (require 'indent-info)
+    (scroll-bar-mode 0)
+    (tool-bar-mode 0)
+    (global-indent-info-mode 1)
+  '';
+
   emacsJail = writeShellScriptBin "emacs-jail" ''
     set -euo pipefail
     export HOME=$(mktemp -d)
-    exec emacs -Q
+    exec emacs -Q -l ${emacsInit}
   '';
+
+  buildInputs = [
+    emacsJail
+    peek
+  ];
 
   emacsWithPackages =
     x: let
@@ -31,30 +44,19 @@ let
 
   shells = {
     dev = mkShell {
-      buildInputs = [
-        emacsJail
-      ];
+      inherit buildInputs;
     };
 
     emacs25 = mkShell {
-      buildInputs = [
-        emacsJail
-        (emacsWithPackages emacs25)
-      ];
+      buildInputs = buildInputs ++ [(emacsWithPackages emacs25)];
     };
 
     emacs26 = mkShell {
-      buildInputs = [
-        emacsJail
-        (emacsWithPackages emacs26)
-      ];
+      buildInputs = buildInputs ++ [(emacsWithPackages emacs26)];
     };
 
     emacsGit = mkShell {
-      buildInputs = [
-        emacsJail
-        (emacsWithPackages emacsGit)
-      ];
+      buildInputs = buildInputs ++ [(emacsWithPackages emacsGit)];
     };
   };
 in shells.dev // shells
